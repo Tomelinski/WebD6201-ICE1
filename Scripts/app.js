382a847e10c4a6faeba4e498012b1eaa63098d9a
Tomelinski <tomzielinski@live.ca>
2021-01-22 13:24:20 -0500

    added bootstrap and font-awesome

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+/* Custom JavaScript goes here */
+/**
+ * Author: Tom Zielinski
+ * Date: 01/21/2021
+ */
+
+//IIFE - Immediatly invoked function expression
+
+(function () {
+    console.log("App started...");
+
+    window.addEventListener("load", Start);
+})();
